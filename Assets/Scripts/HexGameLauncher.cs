ac4b4d8f5a20aa61f3b285233a3c826418823d22
PROgrammer0524 <dco70524@gmail.com>
2020-06-23 14:34:50 +0800

    random change

--- Assets/Scripts/HexGameLauncher.cs
+++ Assets/Scripts/HexGameLauncher.cs
@@ -61,7 +61,7 @@ public class HexGameLauncher : MonoBehaviourPunCallbacks
     public void Create()
     {
         RoomOptions options = new RoomOptions();
-        options.MaxPlayers = 1;
+        options.MaxPlayers = 2;
 
         PhotonNetwork.CreateRoom(roomName.text, options);
     }
